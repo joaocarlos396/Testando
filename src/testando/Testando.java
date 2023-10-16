7bd9d54250f76926e1e73e21f2466cd8367193e6
joaocarlos396 <2022172@student.cct.ie>
2023-10-16 19:28:24 +0100

    Update Testando.java

--- src/testando/Testando.java
+++ src/testando/Testando.java
@@ -14,10 +14,7 @@ public class Testando {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("i am here");
-        System.out.println("whrere");
-        
+        // TODO code application logic here    
     }
     
 }
